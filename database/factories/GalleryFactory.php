66a59b5c8ae32529a2fa25d981c4d7e281eceb92
Jimmy Aldape <jimmy@aldape.dev>
2021-10-31 19:54:04 -0700

    adds gallery factory and seeder

---
+++ database/factories/GalleryFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class GalleryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => Str::ucfirst($this->faker->word),
+            'description' => $this->faker->optional->sentence,
+            'location' => $this->faker->optional->city,
+        ];
+    }
+
+    public function folder(string $folderName)
+    {
+        return $this->state([
+            'folder' => $folderName
+        ]);
+    }
+}
